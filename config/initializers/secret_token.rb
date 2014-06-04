d66333bae8d980503c2f7cbaaa49bcf453ca96b6
Sidney <sidneyquitorio8@yahoo.com>
2014-06-04 13:59:35 -0700

    first commit w/ bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyGift::Application.config.secret_token = '9273dbf51b5a1f6cb8504b55d8460884d974c935e328c8f28904323e5ace2d359b33af0076d76d4fc46fb217be7251320a845cb59f5e01b03f402270a1824119'
