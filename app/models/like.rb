db8afe31bdddc30867a4c0cf9d5b829f471c0cd5
Sidney <sidneyquitorio8@yahoo.com>
2014-06-10 18:59:40 -0700

    Like validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,13 @@ class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :gift_request
   belongs_to :comment
+
+  validate :like_cannot_belong_to_post_and_comment
+
+  def like_cannot_belong_to_post_and_comment
+  	if gift_request_id && comment_id
+  		errors[:base ] << "Like cannot belong to both a gift request post and a comment"
+  	end
+  end
+
 end
