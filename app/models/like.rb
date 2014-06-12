47896a8eef68b6ad529caece4a877b3fccd538ed
Sidney <sidneyquitorio8@yahoo.com>
2014-06-11 17:01:02 -0700

    Like validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -6,6 +6,8 @@ class Like < ActiveRecord::Base
   belongs_to :comment
 
   validate :like_cannot_belong_to_post_and_comment
+  validates_uniqueness_of :comment_id, :allow_nil => true, :scope => :user_id
+  validates_uniqueness_of :gift_request_id, :allow_nil => true, :scope => :user_id
 
   def like_cannot_belong_to_post_and_comment
   	if gift_request_id && comment_id
