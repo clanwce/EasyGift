34a6579cd4b90a62f6f219d696c243be0f38d879
Norman <norman@ubuntu.(none)>
2014-06-15 19:56:30 -0700

    BorderChange

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,11 @@
 class HomeController < ApplicationController
-	#before_filter :authenticate_user!
+	before_filter :authenticate_user!
 
 	def index
 
 	end
 
 	def test
-
-	end 
-
-	def profile
 
 	end
 
