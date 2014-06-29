9d3852afa3588d5eead0992372ee0b3d873acd52
Sidney <sidneyquitorio8@yahoo.com>
2014-06-29 15:58:10 -0700

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ EasyGift::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  # config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
-  # config.assets.initialize_on_precompile = false
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+  config.assets.initialize_on_precompile = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
