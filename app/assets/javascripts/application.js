2f3be2321c6fa1d9bdf3ff7b76c21787fb66226b
Chirag <samkarora@gmail.com>
2014-07-05 21:57:09 -0700

    notification fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-= require jquery
 // = require jquery-ui-1.10.4.custom
 // = require jquery_ujs
 // = require autocomplete-rails
 // = require_tree .
+// = require jquery
 
