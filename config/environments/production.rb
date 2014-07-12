35ec5f1234268d78b9de6e5f604ad2490f75ecc3
Sidney <sidneyquitorio8@yahoo.com>
2014-07-11 21:54:59 -0700

    Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,13 @@ EasyGift::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  # config.assets.compile = true
+
+  # fix
+  config.serve_static_assets = false
+  config.assets.compress = true
+  config.assets.digest = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
