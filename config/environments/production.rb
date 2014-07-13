62ca60b3c0a2a130ad093505be90d7897247f491
Sidney <sidneyquitorio8@yahoo.com>
2014-07-13 12:35:59 -0700

    facebook popup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ EasyGift::Application.configure do
   ENV['FACEBOOK_SECRET'] = '88b4d03810c510ced21861f2026fc68e'
   ENV['FACEBOOK_LOGIN_URL'] = "http://easy-gift.herokuapp.com/auth/facebook"
 
-  ENV['HOMEPAGE_SIGN_IN'] = "http://easy-gift.herokuapp.com/users/sign_in"
+  ENV['HOMEPAGE_SIGN_IN'] = "http://easy-gift.herokuapp.com/"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
