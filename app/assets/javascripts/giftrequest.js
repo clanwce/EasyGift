e9145259405a8cb1daa19875f2d9546046a2adb4
chirag arora <samkarora@gmail.com>
2014-07-13 03:24:20 -0700

    tags search and sort

--- app/assets/javascripts/giftrequest.js
+++ app/assets/javascripts/giftrequest.js
@@ -49,22 +49,50 @@ function onclicktagsearch()
 		var link = "/tags/tag_search";
 		data = {};
 		tag = {};
+
 		tag['keyword'] = $('#autocomplete_tag').val();
+		if(tag['keyword']=="" || tag['keyword'] == null)
+		{
+			$("#divcontainer").html("<h4>No input given</h4>");
+			return false;
+		}
+
 		tag['authenticity_token'] = $('#authenticity_token').val();
-		alert(tag['keyword']);		
 	    $.ajax({
 		    type: "POST",
 		    url: "/tags/tag_search",
 		    data: tag,
 		    success: function(tags) {
+		    	$("#divcontainer").html("");
+		    	var html = "<h4> Tag search for "+$('#autocomplete_tag').val();
+		    	html = html + "</h4><div class='table-responsive'><table class='table'><tr>";
+		     		
+		    	i = 0;
+		    	if(tags ==  null || tags == "")
+		    	{
+		    		$("#divcontainer").html("<h4>No tag found</h4>");
+		    		return false;
+		    	}
 				$.each(tags, function(index,value) {
-				  $.each(value, function(k, v) {
-						
-		   		  });
+
+						if(i==4)
+						{
+							i==0;
+	        				html = html + "</tr><tr>";
+						}
+
+	            		html = html + "<td class='tag-cell'><div class='excerpt'>";
+	            		html = html + "<a href='/tags/'"+value['id']+" class='btn btn-default btn-sm'>"+value['name']+"</a> x";
+	            		html = html + " " +value['gift_request_count'] + "</div></td>";
+	              		i++;
+
 				});
+				html = html + "</tr></table></div>";
+				$("#divcontainer").html(html);
+		    	
 			},
 		    error: function(response) {
-		    	// /alert(response.responseText);
+		    	alert(response.responseText);
 		    }
 	    });	
 
