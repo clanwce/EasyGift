1cc97c694ada873ef468794b4bec4cbb6fcb0225
chirag arora <samkarora@gmail.com>
2014-07-17 03:52:04 -0700

    gift request index js bugfix

--- app/assets/javascripts/giftrequest.js
+++ app/assets/javascripts/giftrequest.js
@@ -3,6 +3,7 @@ function onclickbttn(obj,likestatus)
 	id= document.getElementById(obj);
 	id= obj.id;
 	var link = "/comments/"+id+"/"+likestatus;
+	newText="";
 	// alert(likestatus);
 
 	if(likestatus == "like")
@@ -10,7 +11,8 @@ function onclickbttn(obj,likestatus)
 	$.get( link, function() {
 	})
 	  .done(function(response) {
-
+	  	newText="";
+	  	$('#liketable').html("");
 		$.each(response, function(index,value) {
 		  $.each(value, function(k, v) {
 			newText='<tr class="child"><td><div class="inline"><div style="background-color:#008080;width:45%"><img src="/images/user.png" width="80" heigth="80" class="inline"><button onclick="location.href=\'/users/'+k+'\'" class="inline btn" id="'+k+'">'+v+'</button></td></tr>';
